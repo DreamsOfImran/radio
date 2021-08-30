94f313ded914a43d90f7bc1911ac35e50c70c509
Imran Basha <ibasha66@gmail.com>
2021-08-30 20:40:45 +0530

    Adding console.log

--- api/users.js
+++ api/users.js
@@ -1,9 +1,11 @@
-module.exports = async (req, res) => {
-  if(req.method === "GET") {
-    fetch("https://fr1.api.radio-browser.info/json/stations/search?language=tamil&limit=30&hidebroken=true")
-      .then(response => response.json())
-      .then(data => res.json(data))
-  } else {
 
+
+module.exports = async (req, res) => {
+  console.log(fetch)
+  if(req.method === "GET") {
+    const value = await fetch("https://fr1.api.radio-browser.info/json/stations/search?language=tamil&limit=30&hidebroken=true").then(resp => resp.json())
+    res.json(value)
+  } else {
+    
   }
 }
