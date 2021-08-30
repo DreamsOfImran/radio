e86fa15cb45733ccbea1282e4cb29f7bb4c15288
Imran Basha <ibasha66@gmail.com>
2021-08-30 21:08:44 +0530

    Adding axios

--- api/users.js
+++ api/users.js
@@ -1,6 +1,8 @@
+const axios = require("axios")
+
 module.exports = async (req, res) => {
   if(req.method === "GET") {
-    // console.log(fetch)
+    console.log(axios)
     // const value = await fetch("https://fr1.api.radio-browser.info/json/stations/search?language=tamil&limit=30&hidebroken=true").then(resp => resp.json())
     res.json({ name: "Imran Basha" })
   } else {
