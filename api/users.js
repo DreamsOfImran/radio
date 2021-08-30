212d4903100263474c6ccee9a2f68e7886975ef9
Imran Basha <ibasha66@gmail.com>
2021-08-30 21:06:21 +0530

    Removing fetch on console.log

--- api/users.js
+++ api/users.js
@@ -1,6 +1,6 @@
 module.exports = async (req, res) => {
   if(req.method === "GET") {
-    console.log(fetch)
+    // console.log(fetch)
     // const value = await fetch("https://fr1.api.radio-browser.info/json/stations/search?language=tamil&limit=30&hidebroken=true").then(resp => resp.json())
     res.json({ name: "Imran Basha" })
   } else {
